3480a25e86ec9e285dbd030648439c2c5ab85643
kthomas <thomas.kauffmant@ynov.com>
2023-11-13 15:17:59 +0100

    feat: adding all route

--- src/index.php
+++ src/index.php
@@ -4,13 +4,31 @@ $request = $_SERVER['REQUEST_URI'];
 
 switch ($request) {
   case '/':
-    require __DIR__ . '/views/index.php';
+    require __DIR__ . '/views/home.php';
+    break;
+  case '/home':
+    require __DIR__ . '/views/home.php';
+    break;
+  case '/dashboard':
+    require __DIR__ . '/views/dashboard.php';
     break;
   case '/signin':
     require __DIR__ . '/views/signin.php';
     break;
-  case '/home':
-    require __DIR__ . '/views/home.php';
+  case '/submit':
+    require __DIR__ . '/views/submit.php';
+    break;
+  case '/signup':
+    require __DIR__ . '/views/signup.php';
+    break;
+  case '/post':
+    require __DIR__ . '/views/post.php';
+    break;
+  case '/book':
+    require __DIR__ . '/views/book.php';
+    break;
+  case '/profil':
+    require __DIR__ . '/views/profil.php';
     break;
   default:
     http_response_code(404);
