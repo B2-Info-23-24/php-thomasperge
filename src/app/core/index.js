d3e578cebbc06389496c15d20c692ffa18cffbae
kthomas <thomas.kauffmant@ynov.com>
2023-11-15 22:47:31 +0100

    feat: adding redirection "/cars?id=..." when click on vehicle

--- src/app/core/index.js
+++ src/app/core/index.js
@@ -1,3 +1,9 @@
-document.getElementById('maDiv').addEventListener('click', function() {
-  console.log('La div a été cliquée!');
+var vehicleContainers = document.querySelectorAll('.vehicle-container');
+
+vehicleContainers.forEach(function(container) {
+  container.addEventListener('click', function() {
+      var divId = this.getAttribute('data-id');
+      
+      window.location.href = '/cars?id=' + divId;
+  });
 });
