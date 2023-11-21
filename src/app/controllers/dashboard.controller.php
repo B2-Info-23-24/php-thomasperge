f7848dac91905e6cfd02763bb7d23d1f65052180
kthomas <thomas.kauffmant@ynov.com>
2023-11-21 07:35:36 +0100

    feat: adding display booking

--- src/app/controllers/dashboard.controller.php
+++ src/app/controllers/dashboard.controller.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../core/render.php';
 require_once __DIR__ . '/../models/vehicle.model.php';
 require_once __DIR__ . '/../models/garage.model.php';
 require_once __DIR__ . '/../models/user.model.php';
+require_once __DIR__ . '/../models/booking.model.php';
 
 class DashboardController
 {
@@ -11,6 +12,7 @@ class DashboardController
   private $vehicleModel;
   private $garageModel;
   private $userModel;
+  private $bookingModel;
 
   public function __construct()
   {
@@ -18,6 +20,7 @@ class DashboardController
     $this->userModel = new UserModel($conn);
     $this->garageModel = new GarageModel($conn);
     $this->vehicleModel = new VehicleModel($conn);
+    $this->bookingModel = new BookingModel($conn);
 
     $this->renderManager = new RenderManager();
   }
@@ -29,7 +32,8 @@ class DashboardController
     $userData = $this->userModel->getUserDataFromId($userId);
     $garageData = $this->garageModel->getGarageDataFromUserId($userId);
     $allVehicleFromGarage = $this->vehicleModel->getAllVehicleFromGarageID($garageData[0]['id']);
+    $allBooking = $this->bookingModel->getAllBookingFromGarage($garageData[0]['id']);
 
-    $this->renderManager->render('/pages/dashboard.twig', ['userData' => $userData, 'garageData' => $garageData, 'vehicles' => $allVehicleFromGarage]);
+    $this->renderManager->render('/pages/dashboard.twig', ['userData' => $userData, 'garageData' => $garageData, 'vehicles' => $allVehicleFromGarage, 'allBooking' => $allBooking]);
   }
 }
--- src/app/models/booking.model.php
+++ src/app/models/booking.model.php
@@ -32,4 +32,47 @@ class BookingModel
     $stmt->close();
     return true;
   }
+
+  public function getAllBookingFromGarage($idGarage)
+  {
+    $stmt = $this->conn->prepare('
+        SELECT
+            b.id AS booking_id,
+            b.start_date,
+            b.end_date,
+            b.price AS booking_price,
+            v.id AS vehicle_id,
+            v.brand,
+            v.model,
+            v.url_picture,
+            v.brand_logo,
+            u.firstname,
+            u.lastname
+        FROM
+            booking b
+        JOIN
+            vehicle v ON b.id_vehicle = v.id
+        JOIN
+            users u ON b.id_user = u.id
+        WHERE
+            v.id_garage = ?
+    ');
+
+    if ($stmt === false) {
+      throw new Exception('Erreur de préparation de la requête : ' . $this->conn->error);
+    }
+
+    $stmt->bind_param('s', $idGarage);
+
+    if (!$stmt->execute()) {
+      throw new Exception('Erreur lors de l\'exécution de la requête : ' . $stmt->error);
+    }
+
+    $result = $stmt->get_result();
+    $bookingData = $result->fetch_all(MYSQLI_ASSOC);
+
+    $stmt->close();
+
+    return $bookingData;
+  }
 }
